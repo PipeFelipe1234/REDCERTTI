6afa13cf9ac55fd0bdee1889306c1a00dc5c8acc
PipeFelipe1234 <nominapipe@gmail.com>
2026-02-01 16:05:21 -0500

    Refresh Token logic 3

--- src/main/java/com/practica/backend/security/JwtFilter.java
+++ src/main/java/com/practica/backend/security/JwtFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
